07acd1da86444fe60f33f48bf8813ede83e1a42b
Sundeep <sundeep.kaler@gmail.com>
2023-09-23 21:58:52 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Number.parseInt((Math.random() * 10));
+
+const num3 = 18;
+const num4 = 7;
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 20);
